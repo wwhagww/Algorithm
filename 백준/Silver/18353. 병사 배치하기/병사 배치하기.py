729a6910e470ff0dc7edf2f52dcf28156e242736
wwhagww <167098228+wwhagww@users.noreply.github.com>
2024-04-18 22:32:17 +0900

    [Silver II] Title: 병사 배치하기, Time: 144 ms, Memory: 110864 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -4,7 +4,8 @@ data = list(map(int, input().split()))
 dp = [(0, 1e9), (1, data[0])]
 for i in range(1, n):
     cur = data[i]
-    for l, v in dp[::-1]:
+    for j in range(i, -1, -1):
+        l, v = dp[j]
         if v > cur:
             dp.insert(bisect_left(dp, (l+1, cur)), (l+1, cur))
             break
