b30adf924aa5e33e219b424a4ce95b51f6bb732e
wwhagww <167098228+wwhagww@users.noreply.github.com>
2025-07-18 07:18:34 +0900

    [Gold II] Title: 선분 교차 2, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.py
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.py
@@ -1,57 +1,29 @@
+def ccw(x1, y1, x2, y2, x3, y3):
+    val = (x2 - x1)*(y3 - y1) - (y2 - y1)*(x3 - x1)
+    if val > 0: return 1
+    if val < 0: return -1
+    return 0
+
+def is_cross(x1,y1,x2,y2, x3,y3,x4,y4):
+    d1 = ccw(x1,y1,x2,y2,x3,y3)
+    d2 = ccw(x1,y1,x2,y2,x4,y4)
+    d3 = ccw(x3,y3,x4,y4,x1,y1)
+    d4 = ccw(x3,y3,x4,y4,x2,y2)
+
+    if d1 * d2 <= 0 and d3 * d4 <= 0:
+        if d1 == d2 == d3 == d4 == 0:
+            def in_range(a, b, c):
+                return min(a,b) <= c <= max(a,b)
+            return (
+                in_range(x1,x2,x3) or in_range(x1,x2,x4) or
+                in_range(x3,x4,x1) or in_range(x3,x4,x2)
+            ) and (
+                in_range(y1,y2,y3) or in_range(y1,y2,y4) or
+                in_range(y3,y4,y1) or in_range(y3,y4,y2)
+            )
+        return True
+    return False
+
 x1, y1, x2, y2 = map(int, input().split())
 x3, y3, x4, y4 = map(int, input().split())
-
-def dir(vec, p):
-    # vec 직선 기준 점 p의 위치 판별
-    # 반시계 쪽에 있으면 +1 시계쪽에 있으면 -1 겹치면 0
-    res = None
-    if vec[0] == 0:
-        if p[0] == 0: res = 0
-        if p[0] > 0:
-            if vec[1] > 0: res = -1
-            if vec[1] < 0: res = 1
-        if p[0] < 0:
-            if vec[1] > 0: res = 1
-            if vec[1] < 0: res = -1
-        return res
-
-    check_y = vec[1] * abs(p[0]) * (-1 if (vec[0]>0) ^ (p[0]>0) else 1)
-    mod_y = p[1] * abs(vec[0])
-
-    if mod_y == check_y: res = 0
-    if mod_y > check_y:
-        if vec[0] > 0: res = 1
-        if vec[0] < 0: res = -1
-    if mod_y < check_y:
-        if vec[0] > 0: res = -1
-        if vec[0] < 0: res = 1
-    return res
-
-def is_cross(x1, y1, x2, y2, x3, y3, x4, y4):
-    cross = True
-    d1 = dir((x2-x1, y2-y1), (x3-x1, y3-y1))
-    d2 = dir((x2-x1, y2-y1), (x4-x1, y4-y1))
-    d3 = dir((x4-x3, y4-y3), (x1-x3, y1-y3))
-    d4 = dir((x4-x3, y4-y3), (x2-x3, y2-y3))
-    if d1 == d2:
-        cross = False
-    if d3 == d4:
-        cross = False
-    if d1 == d2 == d3 == d4 == 0:
-        if x1 == x2:
-            if max(y1,y2) < y3 and max(y1,y2) < y4 \
-                or min(y1,y2) > y3 and min(y1,y2) > y4:
-                cross = False
-            else:
-                cross = True
-                
-        else:
-            if max(x1,x2) < x3 and max(x1,x2) < x4 \
-                or min(x1,x2) > x3 and min(x1,x2) > x4:
-                cross = False
-            else:
-                cross = True
-                
-    return cross
-
-print(1 if is_cross(x1, y1, x2, y2, x3, y3, x4, y4) else 0)
+print(1 if is_cross(x1,y1,x2,y2,x3,y3,x4,y4) else 0)
