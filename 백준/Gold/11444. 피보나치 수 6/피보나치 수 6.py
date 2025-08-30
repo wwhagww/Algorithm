3a9d75147e501c36f4326305737bf5c724187f2e
wwhagww <167098228+wwhagww@users.noreply.github.com>
2025-08-31 05:42:02 +0900

    [Gold II] Title: 피보나치 수 6, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -0,0 +1,12 @@
+def fib_mod(n, m):
+    def _fdm(k):
+        if k == 0:
+            return (0, 1)
+        a, b = _fdm(k >> 1)
+        c = (a * ((2*b - a) % m)) % m
+        d = (a*a + b*b) % m
+        return (d, (c + d) % m) if (k & 1) else (c, d)
+    return _fdm(n)[0]
+
+N = int(input())
+print(fib_mod(N, 1_000_000_007))
