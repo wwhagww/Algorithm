b68a64e58bfea892be76483ecf41b1dd3979567b
wwhagww <167098228+wwhagww@users.noreply.github.com>
2025-08-31 00:07:34 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 120 ms, Memory: 113440 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,25 @@
+S, G = map(int, input().split())
+dp = [abs(S-i) for i in range(10**5+1)]
+cur = S
+while cur < G:
+    if cur == 0:
+        dp[1] = 1
+        cur = 1
+        continue
+    for i in range(cur+1, cur*2+1):
+        if i > G: break
+        if i % 2 == 0:
+            dp[i] = min(
+                dp[cur]+abs(cur - i),
+                dp[cur]+abs(cur*2 - i), 
+                dp[i//2]
+                )
+        else:
+            dp[i] = min(
+                dp[cur]+abs(cur-i), 
+                dp[cur]+abs(cur*2-i), 
+                dp[(i+1)//2] + 1,
+                dp[(i-1)//2] + 1 
+                )
+    cur *= 2
+print(dp[G])
