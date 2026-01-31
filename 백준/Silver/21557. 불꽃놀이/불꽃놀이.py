563e53c704eccbf65a172238be88953e65781fd8
wwhagww <167098228+wwhagww@users.noreply.github.com>
2026-02-01 05:06:26 +0900

    [Silver IV] Title: 불꽃놀이, Time: 140 ms, Memory: 141856 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/21557. 불꽃놀이/불꽃놀이.py
+++ 백준/Silver/21557. 불꽃놀이/불꽃놀이.py
@@ -2,7 +2,7 @@ import sys; input=sys.stdin.readline
 N = int(input())
 lst = list(map(int, input().split()))
 H, L = max(lst[0],lst[-1]), min(lst[0],lst[-1])
-if (N-2) < H-L:
+if L-1 <= H-(N-2):
     print(H-(N-2))
 else:
-    print((H+L-(N-2)+1)//2)
+    print((H+L-(N-2))//2)
